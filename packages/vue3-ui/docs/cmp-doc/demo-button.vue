72a0f831a2a2c77a7a5b1be863ca80cbea58b258
NoName4Me <anich.evas@gmail.com>
2022-10-09 14:48:37 +0000

    Deploying to gh-pages from @ my-init/vue-ui@62235e7c8f3015d6a61cec17701946c5ac09c730 🚀

--- packages/vue3-ui/docs/cmp-doc/demo-button.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <DemoButton type="primary" size="big">按钮</DemoButton>
-  <DemoButton type="primary">按钮</DemoButton>
-  <DemoButton type="primary" size="small">按钮</DemoButton>
-</template>
